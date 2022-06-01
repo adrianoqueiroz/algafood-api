0cadbda67c5dd003c41cbda7a0bddf2632c17073
Adriano Novais Queiroz <adriano.nq@gmail.com>
2022-05-31 23:30:30 -0300

    Global exception handler

--- src/main/java/com/algaworks/algafood/api/domain/exception/EntidadeEmUsoException.java
+++ src/main/java/com/algaworks/algafood/api/domain/exception/EntidadeEmUsoException.java
@@ -1,16 +1,13 @@
 package com.algaworks.algafood.api.domain.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
 import java.io.Serial;
 
-@ResponseStatus(HttpStatus.CONFLICT)
 public class EntidadeEmUsoException extends NegocioException{
-  @Serial
-  private static final long serialVersionUID = 1L;
 
-  public EntidadeEmUsoException(String message) {
-    super(message);
-  }
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    public EntidadeEmUsoException(String message) {
+        super(message);
+    }
 }
--- src/main/java/com/algaworks/algafood/api/domain/exception/EntidadeNaoEncontradaException.java
+++ src/main/java/com/algaworks/algafood/api/domain/exception/EntidadeNaoEncontradaException.java
@@ -1,11 +1,7 @@
 package com.algaworks.algafood.api.domain.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
 import java.io.Serial;
 
-@ResponseStatus(value = HttpStatus.NOT_FOUND)
 public abstract class EntidadeNaoEncontradaException extends NegocioException {
 
     @Serial
